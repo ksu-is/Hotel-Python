8eeb44c8e463f0e51f329ec4a71f69f73c2704ad
valeriarojas2 <66020277+valeriarojas2@users.noreply.github.com>
2020-11-20 00:04:34 -0500

    Update hotel.python.py
    I've added a menu option so that the front desk agent knows what to click first as a guest may want to make a booking or change the credit card on file, etc.

--- hotel.python.py
+++ hotel.python.py
@@ -2,3 +2,14 @@ name = []
 phone_number = []
 room_number = []
 reservation = []
+price = []
+customer_id = []
+
+def Home():
+  print("\t Welcome to Hotely Python")
+  print("\t\t 1 Booking")
+  print("\t\t\t 2 Payment")
+  print("\t\t\t\t 3 Guest Requests")
+  print("\t\t\t\t\t 4 Exit")
+  
+Home()
